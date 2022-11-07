4712ee5365c9f6d9cec6208148c124b7db935f68
lijiayuan <lijiayuan07@163.com>
2022-11-07 10:13:24 +0800

    Add version check

---
+++ version.py
@@ -0,0 +1,8 @@
+import torch
+import torchvision
+
+print("pytorch version is", torch.__version__)
+
+print("torchvision version is", torchvision.__version__)
+
+print("cuda version is", torch.cuda_version)
