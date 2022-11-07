7a948d37cbe5e2bce6cb5142a7cb357901d8b1ea
lijiayuan <lijiayuan07@163.com>
2022-11-07 13:39:16 +0800

    Add opencv version check

--- version.py
+++ version.py
@@ -1,8 +1,11 @@
 import torch
 import torchvision
+import cv2
 
 print("pytorch version is", torch.__version__)
 
 print("torchvision version is", torchvision.__version__)
 
 print("cuda version is", torch.cuda_version)
+
+print("open cv version is",cv2.__version__)
